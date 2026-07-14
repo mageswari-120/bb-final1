2c6a2cb52b08bae9dd2b931f117cc9b5640d77d7
mageswari <mageswari@wedesigntech.com>
2026-07-14 15:56:31 +0530

    toml changes done

--- app/routes/auth.$.tsx
+++ app/routes/auth.$.tsx
@@ -1,6 +1,9 @@
-import type { LoaderFunctionArgs } from "@remix-run/node";
+import { json, type LoaderFunctionArgs } from "@remix-run/node";
 import { login } from "../shopify.server";
 
 export async function loader({ request }: LoaderFunctionArgs) {
+  console.log("AUTH ROUTE HIT");
+  console.log(request.url);
+
   return login(request);
 }
